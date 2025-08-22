26c8c437da678d7cf7063911ea427e1f92f84e1b
T. Korn <tobias.korn@towerswatson.com>
2025-08-22 16:04:00 +0200

    Logging G2

---
+++ dotnet/src/AuthTokenHandling.WinFormsClient.net461/CredentialDialog.Designer.cs
@@ -0,0 +1,134 @@
+﻿using System.Windows.Forms;
+
+namespace Security.AccessTokenHandling {
+
+  partial class CredentialDialog {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.checkBox1 = new System.Windows.Forms.CheckBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.Location = new System.Drawing.Point(126, 123);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(105, 26);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "OK";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox1.Location = new System.Drawing.Point(12, 25);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(219, 20);
+      this.textBox1.TabIndex = 1;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox2.Location = new System.Drawing.Point(13, 85);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(218, 20);
+      this.textBox2.TabIndex = 2;
+      this.textBox2.UseSystemPasswordChar = true;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(10, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(37, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Logon";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(12, 69);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(53, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Password";
+      // 
+      // checkBox1
+      // 
+      this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.checkBox1.AutoSize = true;
+      this.checkBox1.Location = new System.Drawing.Point(154, 49);
+      this.checkBox1.Name = "checkBox1";
+      this.checkBox1.Size = new System.Drawing.Size(77, 17);
+      this.checkBox1.TabIndex = 5;
+      this.checkBox1.Text = "Remember";
+      this.checkBox1.UseVisualStyleBackColor = true;
+      // 
+      // button2
+      // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.button2.Location = new System.Drawing.Point(15, 123);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(105, 26);
+      this.button2.TabIndex = 6;
+      this.button2.Text = "Cancel";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // CredentialDialog
+      // 
+      this.ClientSize = new System.Drawing.Size(243, 161);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.checkBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button1);
+      this.Name = "CredentialDialog";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private Button button1;
+    private TextBox textBox1;
+    private TextBox textBox2;
+    private Label label1;
+    private Label label2;
+    private CheckBox checkBox1;
+    private Button button2;
+  }
+
+}
---
+++ dotnet/src/AuthTokenHandling.WinFormsClient.net461/CredentialDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Security.AccessTokenHandling {
+
+  internal partial class CredentialDialog : Form {
+
+    public static bool AskUserForCredentials(
+      string logonNameInputLabel,
+      string logonNameSyntaxRegex,
+      string logonPassInputLabel,
+      bool logonNameAvailable,
+      bool persistNameCheckVisible,
+      ref bool persistNameChecked,
+      string errorMessageToDisplay,
+      ref string logonName,
+      ref byte[] logonPass
+      ) {
+
+      using (CredentialDialog dlg = new CredentialDialog()) {
+
+        //TODO: mainfprom als parent aus static variable holen!!!!
+        dlg.ShowDialog();
+
+
+
+        throw new NotImplementedException();
+
+      }
+    }
+
+    private CredentialDialog() {
+    
+      //this.SuspendLayout();
+      
+      this.InitializeComponent();
+      
+      
+ 
+    }
+
+  }
+
+}
